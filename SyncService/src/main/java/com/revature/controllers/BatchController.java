342d5f6e2ee960871d9fa378b291306de884340d
Andres Toledo <49215828+AndresMToledo@users.noreply.github.com>
2020-07-06 16:02:13 -0400

    Added Cross Origins
    Solved CORS error in angular.

--- SyncService/src/main/java/com/revature/controllers/BatchController.java
+++ SyncService/src/main/java/com/revature/controllers/BatchController.java
@@ -2,6 +2,7 @@ package com.revature.controllers;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,17 +16,20 @@ import reactor.core.scheduler.Schedulers;
 
 /**
  * Controls all endpoints related to batches.
+ * 
  * @author Wei Wu, Andres Mateo Toledo Albarracin, Jose Canela
  */
 @RestController
+@CrossOrigin
 public class BatchController {
 	/**
-	 *	Creates an instance of {@link MessageService} used to send data. 
+	 * Creates an instance of {@link MessageService} used to send data.
 	 */
 	private MessageService messageService;
-	
+
 	/**
 	 * Initializes all services.
+	 * 
 	 * @param messageService sets from bean of type {@link MessageService}.
 	 */
 	public BatchController(MessageService messageService) {
@@ -35,8 +39,10 @@ public class BatchController {
 
 	/**
 	 * Uploads data from a JSON request and sends it to a messaging queue.
+	 * 
 	 * @param data represents the mapped data from a JSON to a list of batches.
-	 * @return a Mono state that attempts upload Batch JSON data and send it to a message queue.
+	 * @return a Mono state that attempts upload Batch JSON data and send it to a
+	 *         message queue.
 	 */
 	@PostMapping("/batch")
 	public Mono<Void> uploadJSON(@RequestBody List<Batch> data) {
--- SyncService/src/main/java/com/revature/controllers/SyncController.java
+++ SyncService/src/main/java/com/revature/controllers/SyncController.java
@@ -1,5 +1,6 @@
 package com.revature.controllers;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,27 +12,31 @@ import reactor.core.scheduler.Schedulers;
 
 /**
  * Controls all endpoints related to Forms.
+ * 
  * @author Wei Wu, Andres Mateo Toledo Albarracin, Jose Canela
  *
  */
 @RestController
+@CrossOrigin
 public class SyncController {
 	/**
-	 *	Creates an instance of {@link MessageService} used to send data. 
+	 * Creates an instance of {@link MessageService} used to send data.
 	 */
 	private MessageService messageService;
 
 	/**
 	 * Initializes all services.
+	 * 
 	 * @param messageService sets from bean of type {@link MessageService}.
 	 */
 	public SyncController(MessageService messageService) {
 		super();
 		this.messageService = messageService;
 	}
-	
+
 	/**
 	 * Triggers Sync process to retrieve new data form Google Sheets.
+	 * 
 	 * @return a Mono state that attempts to trigger the sync process.
 	 */
 	@PostMapping("/sync")
